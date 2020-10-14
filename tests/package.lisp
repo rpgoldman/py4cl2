e78e35d74b38b0a7a8bbafe059754b76bda10eaa
digikar99 <shubhamkarayare@gmail.com>
2020-10-14 22:19:08 +0530

    [fix] regression test for 2a372f7 (add networkx)

--- tests/package.lisp
+++ tests/package.lisp
@@ -3,6 +3,7 @@
 (py4cl2:defpymodule "math" nil :silent t)
 (py4cl2:defpymodule "numpy" nil :lisp-package "NP" :silent t)
 (py4cl2:defpymodule "numpy.random" t :silent t)
+(py4cl2:defpymodule "networkx" nil :lisp-package "NX" :silent t)
 
 (defpackage :py4cl2/tests
   (:use :cl :clunit :py4cl2 :iterate)
